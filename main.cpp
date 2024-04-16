13284fed6b622b766092971b917bff65d084516b
AaronSledge <ahern2118@gmail.com>
2024-04-16 16:06:27 -0700

    test

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string username;
+    string password;
+    cout << "Please Enter Username" << endl;
+
+    cin >> username;
+
+    cout << "Please Enter password" << endl;
+
+    cin >> password;
+
+
+    cout << username << endl;
+    cout << password << endl;
+    return 0;
+}
