a49bbf9b27ee3be833ed3a93e6221e8149bdc16a
AaronSledge <ahern2118@gmail.com>
2024-05-31 02:52:11 -0700

    Better input checking

--- main.cpp
+++ main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 using namespace std;
 
+void passwordChecker(string &password);
+
 int main() {
+   try {
     string username;
     string password;
     cout << "Please Enter Username" << endl;
 
-    cin >> username;
+    if(not(cin >> username)) {
+        throw runtime_error("Could not get username");
+    }
 
     cout << "Please Enter password" << endl;
 
-    cin >> password;
+    if(not(cin >> password)) {
+        throw runtime_error("Could not get password");
+    }
 
-
+    passwordChecker(password);
+    
     cout << username << endl;
     cout << password << endl;
+    }
+    catch(const exception &e) {
+        cout << "Exception " << e.what() << endl;
+    }
     return 0;
+}
+
+void passwordChecker(string &password) {
+    if(isdigit(password[0])) {
+        cout << "password can't start with number plase try again" << endl;
+        if(not(cin >> password)) {
+            throw runtime_error("Could not get password");
+        }
+
+        passwordChecker(password);
+    }
+    return;
 }
