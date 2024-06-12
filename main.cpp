1f56860d68a97fb84316bc5902effa1e17b2f762
AaronSledge <ahern2118@gmail.com>
2024-06-11 20:06:02 -0700

    Add in database system

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
-
+#include <cstdlib>
+#include <sqlite3.h>
 using std::string;
 using std::cin;
 using std::cout;
@@ -10,7 +11,7 @@ using std::runtime_error;
 
 void passwordChecker(string &password);
 
-string  hashFunction(string &password);
+string hashFunction(const string &password);
 
 int main() {
    
@@ -29,13 +30,51 @@ int main() {
     }
 
     passwordChecker(password);
-    
+
     cout << username << endl;
     cout << password << endl;
 
     string hashedpassword = hashFunction(password);
 
     cout << hashedpassword << endl;
+
+    /*const char* DBpassword = std::getenv("DB_PASSWORD");
+    if(DBpassword != std::getenv("DBPASSWORD")) {
+        throw runtime_error("Please set the necessary environment variables.");
+    }*/
+
+    sqlite3* DB;
+    sqlite3_stmt *stmt;
+
+    string sqlstatement = "INSERT INTO Users (username, pass) VALUES (?, ?);";
+
+    int exit = sqlite3_open("login.db", &DB);
+    if(exit != SQLITE_OK) {
+        cout << "Failed to open database: " << sqlite3_errmsg(DB) << endl;
+        return -1;
+    }
+
+    int prepare = sqlite3_prepare_v2(DB, sqlstatement.c_str(), -1, &stmt, NULL);
+
+    if(prepare != SQLITE_OK) {
+        cout << "Failed to prepare statement: " << sqlite3_errmsg(DB) << endl;
+        return -1;
+    }
+
+    sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, hashedpassword.c_str(), -1, SQLITE_STATIC);
+
+    int step = sqlite3_step(stmt);
+
+    if(step != SQLITE_DONE) {
+        cout << "Excution failed: " << sqlite3_errmsg(DB) << endl;
+    }
+    else {
+        cout << "Data inserted successfully" << endl;
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(DB);
     return 0;
 }
 
@@ -51,7 +90,7 @@ void passwordChecker(string &password) {
     return;
 }
 
-string hashFunction(string &password) {
+string hashFunction(const string &password) {
     int hash = 13;
     for(std::size_t i = 0; i < password.length(); ++i) {
         hash = hash * 31 + password.at(i);
